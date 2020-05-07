bdf5250520ca71d5ca0392ce45c2102c380bf6e5
Steve Couch <SGCouch1@gmail.com>
2020-05-06 21:56:20 -0700

    starting graph

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import io
 import pickle
 import os.path
 import time
+import random
 import sys
 import getopt
 import re
@@ -14,6 +15,10 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 import sc2reader
 from trueskill import Rating, rate, rate_1vs1, quality_1vs1
@@ -108,13 +113,13 @@ def main(argv):
     page_token = None
     replay_file_ids = []
     while True:
-        response = service.files().list(
+        response = makeRequestWithExponentialBackoff(service.files().list(
             q=f"'{replay_folder_id}' in parents",
             fields="nextPageToken, files(id)",
-            pageToken=page_token).execute()
+            pageToken=page_token))
         response_list = response['files']
         if test_flag:
-            response_list = response_list[:5]
+            response_list = response_list[:10]
         for f_id in response_list:
             replay_file_ids.append(f_id)
         if test_flag:
@@ -129,25 +134,75 @@ def main(argv):
 
     replays = []
     for i, file in enumerate(replay_file_ids):
-        replay = io.BytesIO(service.files().get_media(
-            fileId=file['id']).execute())
+        replay = io.BytesIO(makeRequestWithExponentialBackoff(service.files().get_media(
+            fileId=file['id'])))
         printProgressBar(i + 1, l, prefix='Loading Replays:',
                          suffix='Complete', length=50)
         replay_object = sc2reader.load_replay(replay)
         if replay_exists(replay_object.unix_timestamp, map(lambda x: x.unix_timestamp, replays)):
-            print(f"deleting file {replay_object.map_name}")
             # service.files().delete(fileId=file['id']).execute()
             continue
         replays.append(replay_object)
 
     replays = sorted(replays, key=lambda x: x.unix_timestamp)
 
-    history = []
+    history = {
+        "steve": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        },
+        "ryan": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        },
+        "kevin": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        },
+        "stephen": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        },
+        "laura": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        },
+        "j": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        },
+        "colin": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        },
+        "bo": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        },
+        "george": {
+            "protoss": [25.0],
+            "terran": [25.0],
+            "zerg": [25.0]
+        }
+    }
+
+    valid_replay_length = 0
+
     for replay in replays:
         pprint(
             f"Date: {datetime.utcfromtimestamp(replay.unix_timestamp).strftime('%Y-%m-%d %H:%M:%S')}")
+        pprint(f"Teams: {replay.teams}")
+        pprint(f"Winner: {replay.winner.players}")
         rating_groups = []
-        if len(replay.teams) is 1 or len(replay.teams[0].players) is 0 or len(replay.teams[1].players) is 0 or (len(replay.teams) is 2 and len(replay.teams[0].players) is 1 and len(replay.teams[1].players) is 1 and (not replay.teams[0].players[0].is_human or not replay.teams[1].players[0].is_human)):
+        if has_computers_on_one_team(replay) or len(replay.teams) is 1 or len(replay.teams[0].players) is 0 or len(replay.teams[1].players) is 0 or (len(replay.teams) is 2 and len(replay.teams[0].players) is 1 and len(replay.teams[1].players) is 1 and (not replay.teams[0].players[0].is_human or not replay.teams[1].players[0].is_human)):
             continue
         for team in replay.teams:
             ratings_group = {}
@@ -168,8 +223,8 @@ def main(argv):
             rated_rating_groups = rate(rating_groups)
         else:
             continue
+        valid_replay_length += 1
         for i, team in enumerate(rated_rating_groups):
-            pprint(f"i: {i}, team: {team}")
             for player, rating in team.items():
                 if player.is_human:
                     setattr(players[player.name],
@@ -178,8 +233,106 @@ def main(argv):
                     setattr(players[f"A.I. ({player.difficulty})"],
                             player.play_race.lower(), rating)
 
-        history.append(rated_rating_groups)
-        pprint(rated_rating_groups)
+        history["steve"]["protoss"].append(steve.protoss)
+        history["steve"]["terran"].append(steve.terran)
+        history["steve"]["zerg"].append(steve.zerg)
+        history["ryan"]["protoss"].append(ryan.protoss)
+        history["ryan"]["terran"].append(ryan.terran)
+        history["ryan"]["zerg"].append(ryan.zerg)
+        history["kevin"]["protoss"].append(kevin.protoss)
+        history["kevin"]["terran"].append(kevin.terran)
+        history["kevin"]["zerg"].append(kevin.zerg)
+        history["stephen"]["protoss"].append(stephen.protoss)
+        history["stephen"]["terran"].append(stephen.terran)
+        history["stephen"]["zerg"].append(stephen.zerg)
+        history["laura"]["protoss"].append(laura.protoss)
+        history["laura"]["terran"].append(laura.terran)
+        history["laura"]["zerg"].append(laura.zerg)
+        history["j"]["protoss"].append(j.protoss)
+        history["j"]["terran"].append(j.terran)
+        history["j"]["zerg"].append(j.zerg)
+        history["colin"]["protoss"].append(colin.protoss)
+        history["colin"]["terran"].append(colin.terran)
+        history["colin"]["zerg"].append(colin.zerg)
+        history["bo"]["protoss"].append(bo.protoss)
+        history["bo"]["terran"].append(bo.terran)
+        history["bo"]["zerg"].append(bo.zerg)
+        history["george"]["protoss"].append(george.protoss)
+        history["george"]["terran"].append(george.terran)
+        history["george"]["zerg"].append(george.zerg)
+
+    x = np.linspace(0, valid_replay_length, 1)
+    fig, ax = plt.subplots()
+
+    pprint(history["steve"]["protoss"])
+
+    ax.plot(range(0, len(history["steve"]["protoss"])),
+            history["steve"]["protoss"], label="steve protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["steve"]["terran"], label="steve terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["steve"]["zerg"], label="steve zerg")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["ryan"]["protoss"], label="ryan protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["ryan"]["terran"], label="ryan terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["ryan"]["zerg"], label="ryan zerg")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["kevin"]["protoss"], label="kevin protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["kevin"]["terran"], label="kevin terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["kevin"]["zerg"], label="kevin zerg")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["stephen"]["protoss"], label="stephen protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["stephen"]["terran"], label="stephen terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["stephen"]["zerg"], label="stephen zerg")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["laura"]["protoss"], label="laura protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["laura"]["terran"], label="laura terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["laura"]["zerg"], label="laura zerg")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["j"]["protoss"], label="j protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["j"]["terran"], label="j terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["j"]["zerg"], label="j zerg")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["colin"]["protoss"], label="colin protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["colin"]["terran"], label="colin terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["colin"]["zerg"], label="colin zerg")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["bo"]["protoss"], label="bo protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["bo"]["terran"], label="bo terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["bo"]["zerg"], label="bo zerg")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["george"]["protoss"], label="george protoss")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["george"]["terran"], label="george terran")
+    # ax.plot(len(history["steve"]["protoss"]),
+    #         history["george"]["zerg"], label="george zerg")
+
+    ax.legend()
+
+    fig.savefig("history")
+
+
+def has_computers_on_one_team(replay):
+    for team in replay.teams:
+        for player in team.players:
+            if player.is_human:
+                break
+            return True
+    return False
 
 
 def replay_exists(replay, replays):
@@ -222,6 +375,26 @@ def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=
     # Print New Line on Complete
     if iteration == total:
         print()
+
+
+def makeRequestWithExponentialBackoff(request):
+    """Wrapper to request Google Analytics data with exponential backoff.
+    The makeRequest method accepts the analytics service object, makes API
+    requests and returns the response. If any error occurs, the makeRequest
+    method is retried using exponential backoff.
+    Args:
+        analytics: The analytics service object
+    Returns:
+        The API response from the makeRequest method.
+    """
+    for n in range(0, 5):
+        try:
+            return request.execute()
+
+        except:
+            time.sleep((2 ** n) + random.random())
+
+    print("There has been an error, the request never succeeded.")
 
 
 if __name__ == '__main__':
